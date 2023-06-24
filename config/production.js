09d161d9adc548daf30443219e90b60e727e5d9f
Luka Galjot <luka.galjot@gmail.com>
2023-06-24 12:46:13 +0200

    Change default public api url

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   ENV: 'production',
   APP_URL: '0.0.0.0',
   APP_PORT: 4000,
-  API_URL: process.env.API_URL || 'http://localhost:4400',
+  API_URL: process.env.API_URL || 'http://rainbow.fri.uni-lj.si:4400/',
   BROWSE_TIMEOUT: 3 * 60 * 1000,
   ADMIN: true,
   API_TOKEN: 'none',
